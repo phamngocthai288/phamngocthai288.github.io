21021099ef91368908b698f46302570911427f65
phamngocthai288 <phamngocthai288@users.noreply.github.com>
2018-11-18 22:54:14 +0700

    hoaa

--- caculator/caculator.js
+++ caculator/caculator.js
@@ -3,42 +3,42 @@ var s='';
 function number1(){
 	x='1';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function number2(){
 	x='2';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function number3(){
 	x='3';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function number4(){
 	x='4';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function number5(){
 	x='5';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function number6(){
 	x='6';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function number7(){
 	x='7';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function number8(){
 	x='8';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function number9(){
 	x='9';
@@ -48,47 +48,50 @@ function number9(){
 function number0(){
 	x='0';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function buttononclear(){
 	s='';
-	document.getElementById("result").innerText = s; 
-	document.getElementById("result1").innerText = '';
+	document.getElementById("caculator").innerText = s; 
+	document.getElementById("caculator1").innerText = '';
 }
 function buttonondel(){
 	x=s.slice(0,s.length-1);
 	s=x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function binhphuong(){
-	
-    document.getElementById("result1").innerText = Math.pow(x,2); 
+	s=s*s;
+	t=s;
+    document.getElementById("caculator1").innerText = t;
 }
 function phantram(){
 	x='%';
 	var t=s+x;
-	document.getElementById("result").innerText = t; 
+	document.getElementById("caculator").innerText = t; 
 	s=s+'/100'
 }
 function buttononmultiplication(){
 	x='*';s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
-function buttonondivi(){
-	x='/';s=s+x;
-	document.getElementById("result").innerText = s; 
-}
-function buttononplus(){
-	x='+';
+function phepchia(){
+	x='/';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
+}
+function canbac3(){
+	s=s*s*s;
+	x=s;
+	document.getElementById("caculator").innerText = x;
+	
 }
 function buttononsub(){
 	x='-';
 	s=s+x;
-	document.getElementById("result").innerText = s; 
+	document.getElementById("caculator").innerText = s; 
 }
 function buttononfinish(){
-	document.getElementById("result1").innerText = eval(s);
+	document.getElementById("caculator1").innerText = eval(s);
 
 }
