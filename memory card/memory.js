6d951be7e363975a17ed80be4b69ac0792cfe855
phamngocthai288 <phamngocthai288@users.noreply.github.com>
2018-12-04 20:12:57 +0700

    10d

---
+++ memory card/memory.js
@@ -0,0 +1,20 @@
+var cards = ['hinh1', 'hinh2', 'hinh3', 'hinh4', 'hinh5', 'hinh6'];
+var current = null;
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
